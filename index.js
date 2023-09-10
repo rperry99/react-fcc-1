36191f28682a28f3e08d263577470aef8de9fc7c
Russ Perry <rperry1886@gmail.com>
2023-09-10 17:56:31 -0400

    Refactor to a custom componment
    I turned the last challenge into a custom component to show how you can basically function out different components

--- index.js
+++ index.js
@@ -2,11 +2,20 @@
 ReactDOM.render(
   <div>
     <h1>This is coming from Index.js.</h1>
-    <p>What dose this mean?</p>
-    <ul>
-      <li>I can use JavaScript to add HTML to my page.</li>
-      <li>I don't have to write HTML manually. Kind of.</li>
-    </ul>
+    <MainContent />
   </div>,
   document.getElementById('root')
 );
+
+// Example of a custom react component
+function MainContent() {
+  return (
+    <div>
+      <p>What dose this mean?</p>
+      <ul>
+        <li>I can use JavaScript to add HTML to my page.</li>
+        <li>I don't have to write HTML manually. Kind of.</li>
+      </ul>
+    </div>
+  );
+}
