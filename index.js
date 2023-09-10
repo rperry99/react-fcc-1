5884bf7f0f4f45cc19e232a40ac213a9fd818df7
Russ Perry <rperry1886@gmail.com>
2023-09-10 17:35:23 -0400

    Challenge 1
    This challenge was to add a UL with 2 LIs in it. I went above and beyond with this, and also included an extra P tag, and kept the original H1

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-ReactDOM.render(<h1>I'm from React!</h1>, document.getElementById("root"))
+// Writing to the DOM with React
+ReactDOM.render(
+  <div>
+    <h1>This is coming from Index.js.</h1>
+    <p>What dose this mean?</p>
+    <ul>
+      <li>I can use JavaScript to add HTML to my page.</li>
+      <li>I don't have to write HTML manually. Kind of.</li>
+    </ul>
+  </div>,
+  document.getElementById('root')
+);
